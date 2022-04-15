22380c92aa28ccf11cb711e8bedc122d44e6136e
PCY71 <skypcy71@gmail.com>
2022-04-16 06:27:18 +0900

    add create, read tweet

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { dbService } from '../fbase';
 
 const Home = () => {
     const [tweet, setTweet] = useState("");
+    const [tweets, setTweets] = useState([]);
     
+    const getTweets = async () =>{
+        const data = await dbService.collection("tweets").get();
+        data.forEach((doc) => {
+            const docObj = {
+                // keys : createAt, tweet, id
+                ...doc.data(),
+                id : doc.id,
+            }
+            setTweets((prev) => [...prev, docObj]);
+        })        
+    };
+
+    useEffect(() =>{
+        getTweets();
+    }, [])
+
     const onSubmit = async (event) =>{
         event.preventDefault();
         await dbService.collection("tweets").add({
@@ -17,12 +34,21 @@ const Home = () => {
         setTweet(value);
     };
 
+    console.log(tweets)
+
     return (
     <div>
         <form onSubmit={onSubmit}>
             <input type="text" placeholder="What's on your mind?" value={tweet} maxLength={120} onChange={onChange} />
             <input type="submit" value="Tweet" />
         </form>
+        <div>
+            {tweets.map(tweet => 
+                    <div key={tweet.id}>
+                        <h4>{tweet.tweet}</h4>
+                    </div>
+                )}
+        </div>
     </div>
     );
 };
