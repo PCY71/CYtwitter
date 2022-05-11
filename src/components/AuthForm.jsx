87dbf92611ea04e6f4cce0197d27b26a3ccf5bed
PCY71 <skypcy71@gmail.com>
2022-05-11 19:04:56 +0900

    auth css

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { authService } from '../fbase';
+import styled from 'styled-components';
 
 const AuthForm = () => {
     const [email, setEmail] = useState("");
     const [newAccount, setNewAccount] = useState(true);
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
-
 
     const toggleAccount = () => setNewAccount((prev) => !prev);
     const onChange = (event) => {
@@ -41,29 +41,60 @@ const AuthForm = () => {
 
     return (
         <>
-            <form onSubmit={onSubmit}>
-                <input
+            <Container onSubmit={onSubmit}>
+                <AuthInput
                     name="email"
                     type="email"
                     placeholder="Email"
                     required value={email}
                     onChange={onChange}
                 />
-                <input
+                <AuthInput
                     name="password"
                     type="password"
                     placeholder="Password"
                     required value={password}
                     onChange={onChange}
                 />
-                <input type='submit' value={newAccount ? "Create Account" : "Log In"} />
-                <div>
-                    {error}
-                </div>
-            </form>
+                <AuthSubmit
+                    type='submit'
+                    value={newAccount ? "Create Account" : "Log In"}
+                />
+                {error && <AuthError>{error}</AuthError>}
+            </Container>
             <span onClick={toggleAccount}>{newAccount ? "Sign in" : "Create Account"}</span>
         </>
     );
 }
 
 export default AuthForm;
+
+const Container = styled.form`
+display: flex;
+width: 100%;
+max-width: 320px;
+flex-direction: column;
+`;
+
+const AuthInput = styled.input`
+max-width: 320px;
+width: 100%;
+padding: 10px;
+border-radius: 30px;
+border: none;
+background-color: rgba(255, 255, 255, 1);
+margin-bottom: 10px;
+font-size: 12px;
+color: black;
+`;
+
+const AuthSubmit = styled(AuthInput)`
+text-align: center;
+background: #04aaff;
+color: white;
+margin-top: 10;
+cursor: pointer;
+`;
+
+const AuthError = styled.span`
+`;
--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,4 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faTwitter,
+    faGoogle,
+    faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+import styled from 'styled-components'
 import AuthForm from '../components/AuthForm';
 import { authService, firebaseInstance } from '../fbase';
 
@@ -12,19 +19,54 @@ const Auth = () => {
             provider = new firebaseInstance.auth.GithubAuthProvider();
         }
 
-        const data = await authService.signInWithPopup(provider);
-        console.log(data)
+        await authService.signInWithPopup(provider);
     }
 
     return (
-        <div>
+        <AuthContainer>
+            <FontAwesomeIcon
+                icon={faTwitter}
+                color={"#04AAFF"}
+                size="3x"
+                style={{ marginBottom: 30 }}
+            />
             <AuthForm />
-            <div>
-                <button name="google" onClick={onSocialClick}>Continue with Google</button>
-                <button name="github" onClick={onSocialClick}>Continue with Github</button>
-            </div>
-        </div>
+            <AuthBtns>
+                <AuthBtn name="google" onClick={onSocialClick}>
+                    Continue with Google <FontAwesomeIcon icon={faGoogle} />
+                </AuthBtn>
+                <AuthBtn name="github" onClick={onSocialClick}>
+                    Continue with Github <FontAwesomeIcon icon={faGithub} />
+                </AuthBtn>
+            </AuthBtns>
+        </AuthContainer>
     );
 };
-
 export default Auth;
+
+//styled
+const AuthContainer = styled.div`
+display: flex;
+flex-direction: column;
+height: 100vh;
+justify-content: center;
+align-items: center;
+`;
+
+const AuthBtns = styled.div`
+display: flex;
+justify-content: space-between;
+width: 100%;
+max-width: 320px;
+`;
+
+const AuthBtn = styled.button`
+cursor: pointer;
+border-radius: 20px;
+border: none;
+padding: 10px 0px;
+font-size: 12px;
+text-align: center;
+width: 150px;
+background: white;
+`;
